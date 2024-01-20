50a2188410ab74b710572ea3ebe169d90fda17dc
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2024-01-20 17:29:06 -0500

    mobile drop our menu design

--- js/main.js
+++ js/main.js
@@ -24,6 +24,13 @@ function open(evt) {
     .classList.add("tabs__content-item--active");
 }
 
+const menuBtn = document.querySelector(".menu__btn");
+const menu = document.querySelector(".menu__list");
+
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("menu__list--active");
+});
+
 const swiper = new Swiper(".swiper", {
   effect: "fade",
   pagination: {
